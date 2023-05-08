43d305023cf0b551dade35b248a659c9232f1851
Dominykas Kisonas <dominykas.kisonas@citadele.lv>
2023-05-08 20:33:29 +0300

    added theme php files, updated .gitignore

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <!-- Your footer content here -->
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+/**
+ * The main template file.
+ *
+ * @package My_Theme
+ */
+
+get_header();
+
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+		the_content();
+	endwhile;
+endif;
+
+get_footer();
